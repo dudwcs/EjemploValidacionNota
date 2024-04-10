ff19f7ea3ed027b358da22aac67959bc237b55c8
dwcs-2324 <dwcs2324@gmail.com>
2024-04-10 17:26:50 +0200

    A8.2

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -11,19 +11,26 @@ class LuckyController extends AbstractController
     #[Route('/lucky', name: 'app_lucky')]
     public function index(): Response
     {
-        return $this->render('lucky/index.html.twig', [
-            'controller_name' => 'LuckyController',
-        ]);
+        // return $this->render('lucky/index.html.twig', [
+        //     'controller_name' => 'LuckyController',
+        // ]);
+       return $this->redirectToRoute('app_lucky_number', ['max' => 20]);
     }
 
     #[Route('/lucky/number/{max}', name: 'app_lucky_number')]
     public function maxRandom(int $max): Response
     {
-        $number = random_int(0, $max);
+        //si es impar
+        if($max % 2 == 1){
+            return $this->redirectToRoute('app_table', ['filas' => $max, 'cols' => $max ]);
+        } else {
 
-        return $this->render('lucky/index.html.twig', [
-            'controller_name' => 'LuckyController',
-            'numero' => $number
-        ]);
+            $number = random_int(0, $max);
+
+            return $this->render('lucky/index.html.twig', [
+                'controller_name' => 'LuckyController',
+                'numero' => $number
+            ]);
+        }
     }
 }
